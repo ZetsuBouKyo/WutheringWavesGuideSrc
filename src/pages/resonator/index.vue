444560f72b5c5338decc3e9d361d9cf432ce3a07
ZetsuBouKyo <116266262+ZetsuBouKyo@users.noreply.github.com>
2025-02-15 23:17:44 +0800

    feat: center the resonator icons

--- src/pages/resonator/index.vue
+++ src/pages/resonator/index.vue
@@ -1,22 +1,20 @@
 <template>
   <v-container>
     <h1 class="mb-4">{{ $t('resonators.damage_analysis') }}</h1>
-    <v-row>
-      <v-sheet class=" d-flex flex-wrap justify-start">
-        <v-tooltip :text="`${no} ${resonators.getNameByNo(no)}`" v-for="no in calculatedResonators" :key="no">
-          <template v-slot:activator="{ props }">
-            <v-card class="ma-2 pa-2" v-bind="props" :to="`/resonator/${no}`">
-              <v-img width="100" :src="resonators.getIconSrcByNo(no)"></v-img>
-              <v-row no-gutters align="center" justify="center">
-                <img :src="resonators.getElementSrcByNo(no)" height="36">
-                <span class="mr-2 text-truncate name">{{
-                  resonators.getNameByNo(no) }}</span>
-              </v-row>
-            </v-card>
-          </template>
-        </v-tooltip>
-      </v-sheet>
-    </v-row>
+    <v-sheet class=" d-flex flex-wrap justify-center">
+      <v-tooltip :text="`${no} ${resonators.getNameByNo(no)}`" v-for="no in calculatedResonators" :key="no">
+        <template v-slot:activator="{ props }">
+          <v-card class="ma-2 pa-2" v-bind="props" :to="`/resonator/${no}`">
+            <v-img width="100" :src="resonators.getIconSrcByNo(no)"></v-img>
+            <v-row no-gutters align="center" justify="center">
+              <img :src="resonators.getElementSrcByNo(no)" height="36">
+              <span class="mr-2 text-truncate name">{{
+                resonators.getNameByNo(no) }}</span>
+            </v-row>
+          </v-card>
+        </template>
+      </v-tooltip>
+    </v-sheet>
   </v-container>
 </template>
 
