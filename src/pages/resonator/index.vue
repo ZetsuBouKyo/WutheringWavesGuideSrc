c258ae9d1dede52083b330ce66b1c30e9e6e2f66
ZetsuBouKyo <116266262+ZetsuBouKyo@users.noreply.github.com>
2025-02-16 00:14:14 +0800

    refactor: calculated resonator ids

--- src/pages/resonator/index.vue
+++ src/pages/resonator/index.vue
@@ -2,14 +2,15 @@
   <v-container>
     <h1 class="mb-4">{{ $t('resonators.damage_analysis') }}</h1>
     <v-sheet class=" d-flex flex-wrap justify-center">
-      <v-tooltip :text="`${no} ${resonators.getNameByNo(no)}`" v-for="no in calculatedResonators" :key="no">
+      <v-tooltip :text="`${id} ${resonators.getNameByID(id)}`"
+        v-for="id in calculatedResonators.getCalculatedResonatorIDs()" :key="id">
         <template v-slot:activator="{ props }">
-          <v-card class="ma-2 pa-2" v-bind="props" :to="`/resonator/${no}`">
-            <v-img width="100" :src="resonators.getIconSrcByNo(no)"></v-img>
+          <v-card class="ma-2 pa-2" v-bind="props" :to="`/resonator/${id}`">
+            <v-img width="100" :src="resonators.getIconSrcByID(id)"></v-img>
             <v-row no-gutters align="center" justify="center">
-              <img :src="resonators.getElementSrcByNo(no)" height="36">
+              <img :src="resonators.getElementSrcByID(id)" height="36">
               <span class="mr-2 text-truncate name">{{
-                resonators.getNameByNo(no) }}</span>
+                resonators.getNameByID(id) }}</span>
             </v-row>
           </v-card>
         </template>
@@ -19,8 +20,7 @@
 </template>
 
 <script lang="ts" setup>
-import calculatedResonators from "@/assets/calculation/resonators.json"
-import { resonators } from "@/ww/resonator"
+import { resonators, calculatedResonators } from "@/ww/resonator"
 </script>
 
 <style scoped lang="sass">
--- src/ww/resonator.ts
+++ src/ww/resonator.ts
@@ -15,35 +15,65 @@ function mapValueToValue(modules: any, key1: string, key2: string): any {
 
 export class Resonators {
   private infoJsonModules: any;
-  private noToElementEn: any;
-  private noToName: any;
+  private nameToID: any;
+  private idToElementEn: any;
+  private idToName: any;
 
   constructor() {
     this.infoJsonModules = import.meta.glob("@/assets/data/resonators/*/info.json", { eager: true });
-    this.noToElementEn = mapValueToValue(this.infoJsonModules, "default.no", "default.element_en");
-    this.noToName = mapValueToValue(this.infoJsonModules, "default.no", "default.name");
+    this.idToElementEn = mapValueToValue(this.infoJsonModules, "default.no", "default.element_en");
+    this.idToName = mapValueToValue(this.infoJsonModules, "default.no", "default.name");
+    this.nameToID = mapValueToValue(this.infoJsonModules, "default.name", "default.no");
   }
 
   public getInfoJsonModules(): Record<string, JsonData> {
     return this.infoJsonModules;
   }
 
-  public getNameByNo(no: string): string {
-    return this.noToName[no];
+  public getNameByID(id: string): string {
+    return this.idToName[id];
   }
 
-  public getElementEnByNo(no: string): string {
-    return this.noToElementEn[no];
+  public getElementEnByID(id: string): string {
+    return this.idToElementEn[id];
   }
 
-  public getElementSrcByNo(no: string): string {
-    const element = this.getElementEnByNo(no);
+  public getElementSrcByID(id: string): string {
+    const element = this.getElementEnByID(id);
     return new URL(`../assets/elements/${element}.png`, import.meta.url).href;
   }
 
-  public getIconSrcByNo(no: string): any {
-    return new URL(`../assets/resonators/${no}/icon.png`, import.meta.url).href;
+  public getIconSrcByID(id: string): any {
+    return new URL(`../assets/resonators/${id}/icon.png`, import.meta.url).href;
+  }
+
+  public getNoByName(name: string): string {
+    return this.nameToID[name];
   }
 }
 
 export const resonators = new Resonators();
+
+export class CalculatedResonators {
+  private templatesModule: any;
+
+  constructor() {
+    this.templatesModule = import.meta.glob("@/assets/calculation/templates.json", { eager: true });
+  }
+
+  public getCalculatedResonatorIDs(): Array<string> {
+    const s: Set<string> = new Set([]);
+    const templates: any = Object.values(this.templatesModule)[0];
+    for (const template of templates.templates) {
+      for (const name of template.echo_comparison) {
+        const id = resonators.getNoByName(name);
+        s.add(id);
+      }
+    }
+    const arr = Array.from(s);
+    arr.sort();
+    return arr;
+  }
+}
+
+export const calculatedResonators = new CalculatedResonators();
