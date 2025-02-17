dec563743d076233bb80472c9d6f9f58881b9a14
ZetsuBouKyo <116266262+ZetsuBouKyo@users.noreply.github.com>
2025-02-17 15:17:23 +0800

    feat: add element colors

--- src/plugins/vuetify.ts
+++ src/plugins/vuetify.ts
@@ -5,15 +5,27 @@
  */
 
 // Styles
-import '@mdi/font/css/materialdesignicons.css'
-import 'vuetify/styles'
+import "@mdi/font/css/materialdesignicons.css";
+import "vuetify/styles";
 
 // Composables
-import { createVuetify } from 'vuetify'
+import { createVuetify } from "vuetify";
 
 // https://vuetifyjs.com/en/introduction/why-vuetify/#feature-guides
 export default createVuetify({
   theme: {
-    defaultTheme: 'dark',
+    defaultTheme: "dark",
+    themes: {
+      dark: {
+        colors: {
+          glacio: "rgb(72, 172, 252)",
+          fusion: "rgb(248, 116, 76)",
+          electro: "rgb(184, 108, 252)",
+          aero: "rgb(88, 252, 180)",
+          spectro: "rgb(255, 228, 108)",
+          havoc: "rgb(208, 140, 180)",
+        },
+      },
+    },
   },
-})
+});
