4937bcf220b81ba09c167531d2b2c8f8f58da95d
ZetsuBouKyo <116266262+ZetsuBouKyo@users.noreply.github.com>
2025-02-18 19:27:13 +0800

    refactor: separate the functions

--- src/ww/template.ts
+++ src/ww/template.ts
@@ -1,25 +1,57 @@
 import { resonators } from "./resonator";
+import { mapValueToValue } from "./utils";
+export class Templates {
+  private hashedTemplateIDToTemplate: any;
+  private hashedTemplateIDToTemplateID: any;
+  private templateIDToHashedTemplateID: any;
 
+  constructor() {
+    const templateModules = import.meta.glob("@/assets/templates/*.json", { eager: true });
+
+    this.templateIDToHashedTemplateID = mapValueToValue(templateModules, "default.id", "default.hashed_id");
+    this.hashedTemplateIDToTemplate = mapValueToValue(templateModules, "default.hashed_id", "default");
+    this.hashedTemplateIDToTemplateID = mapValueToValue(templateModules, "default.hashed_id", "default.id");
+  }
+
+  public getTemplateByHashedTemplateID(hashedTemplateID: string): any {
+    return this.hashedTemplateIDToTemplate[hashedTemplateID];
+  }
+
+  public getTemplateIDByHashedTemplateID(hashedTemplateID: string): string {
+    return this.hashedTemplateIDToTemplateID[hashedTemplateID];
+  }
+
+  public getHashedTemplateIDByTemplateID(templateID: string): string {
+    return this.templateIDToHashedTemplateID[templateID];
+  }
+}
+
+export const templates = new Templates();
 export class CalculatedTemplates {
   private templates: any;
-  private hashedComparisonTitleToTemplateIDs: any;
-  private templateIDToAffixes151DamageAnalysis: any;
-  private templateIDToHashedID: any;
+  private hashedComparisonTitleToComparison: any;
+  private hashedTemplateIDToResonatorsForEchoComparison: any;
 
   constructor() {
     const templatesModule = import.meta.glob("@/assets/calculation/templates.json", { eager: true });
     this.templates = Object.values(templatesModule)[0];
 
-    this.templateIDToHashedID = {};
+    this.hashedTemplateIDToResonatorsForEchoComparison = {};
     this.templates.templates.forEach((template: any) => {
-      this.templateIDToHashedID[template.id] = template.hashed_id;
+      const resonatorNames: Array<string> = template.echo_comparison;
+      const r: Array<any> = [];
+      resonatorNames.forEach((name: string) => {
+        const id = resonators.getIDByName(name);
+        r.push({ id: id, name: name });
+      });
+      this.hashedTemplateIDToResonatorsForEchoComparison[template.hashed_id] = r;
     });
 
-    this.hashedComparisonTitleToTemplateIDs = {};
+    this.hashedComparisonTitleToComparison = {};
     Object.keys(this.templates.comparisons).forEach((name: string) => {
       const comparisons: any = this.templates.comparisons[name];
       comparisons.forEach((comparison: any) => {
-        this.hashedComparisonTitleToTemplateIDs[comparison.id] = comparison.template_ids;
+        this.hashedComparisonTitleToComparison[comparison.id] = comparison;
       });
     });
   }
@@ -37,6 +69,10 @@ export class CalculatedTemplates {
     return arr;
   }
 
+  public getResonatorsForEchoComparison(hashedTemplateID: string): Array<{ id: string; name: string }> {
+    return this.hashedTemplateIDToResonatorsForEchoComparison[hashedTemplateID];
+  }
+
   public getTemplateIDsByResonatorID(resonatorID: string): Array<string> {
     const ids: Array<string> = [];
     for (const template of this.templates.templates) {
@@ -50,10 +86,6 @@ export class CalculatedTemplates {
     return ids;
   }
 
-  public getHashedTemplateIDByTemplateID(templateID: string): string {
-    return this.templateIDToHashedID[templateID];
-  }
-
   public getComparisonsByID(resonatorID: string): any {
     for (const [name, comparisons] of Object.entries(this.templates.comparisons)) {
       const id = resonators.getIDByName(name);
@@ -64,8 +96,13 @@ export class CalculatedTemplates {
     return [];
   }
 
+  public getComparisonByComparisonID(id: string): any {
+    return this.hashedComparisonTitleToComparison[id];
+  }
+
   public getTemplateIDsByComparisonID(id: string): Array<string> {
-    return this.hashedComparisonTitleToTemplateIDs[id];
+    const comparison = this.getComparisonByComparisonID(id);
+    return comparison.template_ids;
   }
 }
 
