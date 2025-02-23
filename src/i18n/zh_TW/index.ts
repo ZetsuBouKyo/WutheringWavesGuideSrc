8642e02fcf583b97ef994223ce14ac7decc53456
ZetsuBouKyo <116266262+ZetsuBouKyo@users.noreply.github.com>
2025-02-23 11:26:26 +0800

    feat: add weapon locales

--- src/i18n/zh_TW/index.ts
+++ src/i18n/zh_TW/index.ts
@@ -1,4 +1,5 @@
 import resonatorName from "./resonator/name.json";
+import weaponName from "./weapon/name.json";
 import coparison_titles from "./comparison_titles.json";
 import echo_policies from "./echo_policies.json";
 import general from "./general.json";
@@ -6,10 +7,11 @@ import monster from "./monster.json";
 import template_ids from "./template_ids.json";
 
 export const zh_TW: any = {
+  ...resonatorName,
+  ...weaponName,
   ...coparison_titles,
   ...echo_policies,
   ...general,
   ...monster,
   ...template_ids,
-  ...resonatorName,
 };
