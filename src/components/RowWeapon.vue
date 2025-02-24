9a36b40b8b1d29b1ed1a7a5579ef5211eb500826
ZetsuBouKyo <116266262+ZetsuBouKyo@users.noreply.github.com>
2025-02-24 19:35:24 +0800

    feat: change the component

--- src/components/RowWeapon.vue
+++ src/components/RowWeapon.vue
@@ -1,8 +1,8 @@
 <template>
   <div class="d-flex flex-column w-100">
     <div class="d-flex flex-row">
-      <v-select v-model="weapon.name" :items="weaponNames" :label="$t('general.name')"
-        @update:modelValue="weapon.updateRowWeaponByName()"></v-select>
+      <v-combobox v-model="weapon.name" :items="weaponNames" :label="$t('general.name')" :rules="[checkWeaponName]"
+        @update:modelValue="weapon.updateRowWeaponByName()"></v-combobox>
     </div>
     <div class="d-flex flex-row">
       <v-select v-model="weapon.level" :items="weaponLevels" :label="$t('general.level')"></v-select>
@@ -14,6 +14,8 @@
 </template>
 
 <script lang="ts" setup>
+import { useI18n } from 'vue-i18n'
+
 import useRowWeaponStore from '@/stores/calculation/weapon';
 
 import { useWeaponStore } from '@/stores/weapon';
@@ -27,10 +29,19 @@ const props = defineProps({
 
 const id = props.id
 
+const { t } = useI18n()
+
 const weaponStore = useWeaponStore()
 const weaponNames = weaponStore.getNames()
 const weaponLevels = weaponStore.getLevels()
 const weaponTunes = weaponStore.getTunes()
 
 const weapon = useRowWeaponStore(id)
+
+function checkWeaponName() {
+  if (weaponNames.includes(name)) {
+    return true
+  }
+  return t('general.error')
+}
 </script>
