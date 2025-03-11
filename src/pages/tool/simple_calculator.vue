b33e3ab8ae719b9240db710f353fdfcaf9cf641c
ZetsuBouKyo <116266262+ZetsuBouKyo@users.noreply.github.com>
2025-03-11 18:06:32 +0800

    feat: update user interface

--- src/pages/tool/simple_calculator.vue
+++ src/pages/tool/simple_calculator.vue
@@ -21,13 +21,13 @@
         <v-tabs-window-item value="resonator">
           <div class="d-flex flex-row w-100">
             <div class="d-flex flex-column w-100 mr-6">
-              <div class="d-flex flex-row mb-2">
+              <div class="d-flex flex-row mb-4">
                 <h2>{{ $t('general.resonator') }}</h2>
               </div>
               <RowResonator :id="id" />
             </div>
             <div class="d-flex flex-column w-100">
-              <div class="d-flex flex-row mb-2">
+              <div class="d-flex flex-row mb-4">
                 <h2>{{ $t('general.weapon') }}</h2>
               </div>
               <RowWeapon :id="id" />
@@ -37,17 +37,17 @@
         <v-tabs-window-item value="echo">
           <div class="d-flex flex-row w-100">
             <div class="d-flex flex-column w-100 mr-6">
-              <div class="d-flex flex-row mb-2">
+              <div class="d-flex flex-row mb-4">
                 <h2>{{ $t('general.auto_fill') }}</h2>
               </div>
               <RowAutoFillEcho :id="id" />
             </div>
             <div class="d-flex flex-column w-100">
-              <div class="d-flex flex-row mb-2">
+              <div class="d-flex flex-row mb-4">
                 <h2>{{ $t('general.manual_input') }}</h2>
               </div>
               <div v-for="(_, i) in echoes.data.echoes" class="d-flex flex-column w-100">
-                <div class="d-flex flex-row mb-2">
+                <div class="d-flex flex-row mb-4">
                   <h3>{{ $t('general.nth_echo', { n: i + 1 }) }}</h3>
                 </div>
                 <RowEcho :i="i" :id="id" />
@@ -58,7 +58,7 @@
         <v-tabs-window-item value="monster">
           <div class="d-flex flex-row w-100">
             <div class="d-flex flex-column w-100 mr-6">
-              <div class="d-flex flex-row mb-2">
+              <div class="d-flex flex-row mb-4">
                 <h2>{{ $t('general.monster') }}</h2>
               </div>
               <RowMonster :id="id" />
@@ -70,12 +70,12 @@
         <v-tabs-window-item value="buff">
           <div class="d-flex flex-row w-100">
             <!-- <div class="d-flex flex-column w-100 mr-6">
-              <div class="d-flex flex-row mb-2">
+              <div class="d-flex flex-row mb-4">
                 <h2>{{ $t('general.quick_input') }}</h2>
               </div>
             </div> -->
             <div class="d-flex flex-column w-100">
-              <div class="d-flex flex-row mb-2">
+              <div class="d-flex flex-row mb-4">
                 <h2>{{ $t('general.manual_input') }}</h2>
               </div>
               <RowManualBuff :id="id" />
@@ -85,29 +85,29 @@
         <v-tabs-window-item value="result">
           <div class="d-flex flex-row w-100">
             <div class="d-flex flex-column w-100 mr-6">
-              <div class="d-flex flex-row mb-2">
+              <div class="d-flex flex-row mb-4">
                 <h2>{{ $t('general.resonator') }}</h2>
               </div>
               <RowResonatorResult :id="id" />
-              <div class="d-flex flex-row mb-2">
+              <div class="d-flex flex-row mb-4">
                 <h2>{{ $t('general.weapon') }}</h2>
               </div>
               <RowWeaponResult :id="id" />
-              <div class="d-flex flex-row mb-2">
+              <div class="d-flex flex-row mb-4">
                 <h2>{{ $t('general.echo') }}</h2>
               </div>
               <RowEchoResult :id="id" />
-              <div class="d-flex flex-row mb-2">
+              <div class="d-flex flex-row mb-4">
                 <h2>{{ $t('general.monster') }}</h2>
               </div>
               <RowMonsterResult :id="id" />
-              <div class="d-flex flex-row mb-2">
+              <div class="d-flex flex-row mb-4">
                 <h2>{{ $t('general.buff') }}</h2>
               </div>
               <RowBuffResult :id="id" />
             </div>
             <div class="d-flex flex-column w-100">
-              <div class="d-flex flex-row mb-2">
+              <div class="d-flex flex-row mb-4">
                 <h2>{{ $t('general.result') }}</h2>
               </div>
               <RowDetailedDamage v-if="calculation" :calculation="calculation" />
