46509c32d08277111c07fd74d8ee2c42ff74cd2b
ZetsuBouKyo <116266262+ZetsuBouKyo@users.noreply.github.com>
2025-07-29 19:31:25 +0800

    fix: avoid TS7053 by correcting the type

--- src/stores/sonata.ts
+++ src/stores/sonata.ts
@@ -1,8 +1,11 @@
+import { type ISonataInfo } from "@/types/sonata";
+
 import sonata_info from "@/assets/data/echo/sonatas.json";
 
 export class SonataInfo {
   public name: string = "";
   public piece_2: string = "";
+  public piece_3: string = "";
   public piece_5: string = "";
 
   public getHtmlInfo(): string {
@@ -13,6 +16,9 @@ export class SonataInfo {
     if (this.piece_2) {
       text = `${text}2/2\n${this.piece_2}\n\n`;
     }
+    if (this.piece_3) {
+      text = `${text}3/3\n${this.piece_3}\n\n`;
+    }
     if (this.piece_5) {
       text = `${text}5/5\n${this.piece_5}`;
     }
@@ -22,7 +28,7 @@ export class SonataInfo {
 }
 
 export function getSonataInfo(name: string): SonataInfo | undefined {
-  const data = sonata_info[name as keyof typeof sonata_info];
+  const data = sonata_info[name as keyof typeof sonata_info] as ISonataInfo;
   if (!data) {
     return;
   }
@@ -30,6 +36,7 @@ export function getSonataInfo(name: string): SonataInfo | undefined {
   const info = new SonataInfo();
   info.name = name;
   info.piece_2 = data["2"] ? data["2"] : "";
+  info.piece_3 = data["3"] ? data["3"] : "";
   info.piece_5 = data["5"] ? data["5"] : "";
   return info;
 }
--- src/types/sonata.ts
+++ src/types/sonata.ts
@@ -14,3 +14,9 @@ export const SonataEnum = {
   EMPYREAN_ANTHEM: "高天共奏之曲",
   TIDEBREAKING_COURAGE: "無懼浪濤之勇",
 } as const;
+
+export interface ISonataInfo {
+  2: string;
+  3: string;
+  5: string;
+}
